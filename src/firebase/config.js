31562fa971ec220bce65ee8f2a4a55f550a47524
Egor Cravcenco <cregordudes@gmail.com>
2022-10-23 11:45:26 +0300

    created register page

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,14 +1,8 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import {
-  getFirestore,
-  query,
-  getDocs,
-  collection,
-  where,
-  addDoc,
-} from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCzTyRRBJoourS9t5i7FJ3xjF1YdeH9xm0",
@@ -22,3 +16,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
