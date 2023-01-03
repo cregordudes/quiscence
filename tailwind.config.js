971be6faf91c93a04697da51f1f3a01dc6f4b827
Egor Cravcenco <cregordudes@gmail.com>
2023-01-03 16:14:37 +0200

    added config to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors: {
-        myPink: "#E68BFF",
-        myYellow: "#FFD438",
-        myGreen: "#6BD9A9",
-        myPurple: "##6366f1",
-        gradientFrom: "#464a80",
-        gradientTo: "#2d2f6f",
+   content: [
+      "./pages/**/*.{js,ts,jsx,tsx}",
+      "./src/components/**/*.{js,ts,jsx,tsx}",
+   ],
+   theme: {
+      extend: {
+         colors: {
+            myPink: "#E68BFF",
+            myYellow: "#FFD438",
+            myGreen: "#6BD9A9",
+            myPurple: "##6366f1",
+            gradientFrom: "#464a80",
+            gradientTo: "#2d2f6f",
+         },
+         height: {
+            128: "32rem",
+         },
       },
-    },
-  },
-  plugins: [],
+   },
+   plugins: [],
 };
